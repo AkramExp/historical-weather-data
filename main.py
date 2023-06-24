55d989ce51b378a0eb82d31e4d29e5f0c2788ba1
Akram Ali <akram.exp02@gmail.com>
2023-06-24 21:00:03 +0530

    minor changes main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temp = 20
+    return {'station': station,
+            'date': date,
+            'temperature': temp}
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
